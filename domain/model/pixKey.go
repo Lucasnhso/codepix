c4f1cae31dbb602662878b0a6f23abe83cced17a
Lucas Oliveira <lucasnhso@gmail.com>
2023-10-17 10:57:54 -0300

    add gorm definitions to pixKey model

--- domain/model/pixKey.go
+++ domain/model/pixKey.go
@@ -20,7 +20,7 @@ type PixKey struct {
 	Base `valid:"required"`
 	Kind string `json:"kind" valid:"notnull"`
 	Key string `json:"key" valid:"notnull"`
-	AccountId string `json:"account_id" valid:"notnull"`
+	AccountID string `gorm:"column:account_id;type:uuid;not null" valid:"-"`
 	Account *Account `valid:"-"`
 	Status string `json:"status" valid:"notnull"`
 }
@@ -46,7 +46,7 @@ func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
 		Kind: kind,
     Key: key,
     Account: account,
-		AccountId: account.ID,
+		AccountID: account.ID,
     Status: "active",
 	}
 
